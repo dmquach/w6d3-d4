7c7ca9f7ccb42b1832f911280cf007b121d07dc9
Dylan <dylanquach@gmail.com>
2023-08-23 17:30:25 -0700

    Finish art_share phase_one

--- first_routes/app/models/user.rb
+++ first_routes/app/models/user.rb
@@ -14,4 +14,13 @@ class User < ApplicationRecord
     foreign_key: :artist_id,
     class_name: :Artwork,
     dependent: :destroy
+
+    has_many :artwork_shares,
+    foreign_key: :viewer_id,
+    class_name: :ArtworkShare,
+    dependent: :destoy
+
+    has_many :shared_artworks,
+    through: :artwork_shares,
+    source: :artwork
 end
